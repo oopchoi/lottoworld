daabe53cdc183cfe6856007cfe9b659e442f8a7a
oopchoi <oopchoi@gmail.com>
2015-11-06 16:33:22 +0900

    Update WonEliminator.java

--- lotto-standalone/src/com/xharu/combination/WonEliminator.java
+++ lotto-standalone/src/com/xharu/combination/WonEliminator.java
@@ -55,6 +55,30 @@ public class WonEliminator implements Eliminator {
         return list;
     }
 
+    public ArrayList<ArrayList> removeUnderCount(int underCount, ArrayList<ArrayList> list, ArrayList exceptList) {
+        for(int i=list.size()-1; i>=0; i--) {
+            ArrayList item = list.get(i);
+            for (int m = 0; m < exceptList.size(); m++) {
+                ArrayList exceptItem = (ArrayList)exceptList.get(m);
+                int count = 0;
+                for(int j=0; j<item.size(); j++){
+                    int x = (int) item.get(j);
+                    for(int n=0; n<exceptItem.size(); n++) {
+                        int y = (int) exceptItem.get(n);
+                        if (x == y) {
+                            count++;
+                        }
+                    }
+                }
+                if(count<underCount){
+                    list.remove(i);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+
     public ArrayList<ArrayList> getOnlyCount(int fixCount, ArrayList<ArrayList> list, ArrayList exceptList) {
         ArrayList result = new ArrayList();
         for(int i=list.size()-1; i>=0; i--) {
