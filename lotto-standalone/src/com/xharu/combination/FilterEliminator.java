9fccc2a0491e1673a8480046985d7a39919c0b26
oopchoi <oopchoi@gmail.com>
2015-11-06 16:34:44 +0900

    Update FilterEliminator.java

--- lotto-standalone/src/com/xharu/combination/FilterEliminator.java
+++ lotto-standalone/src/com/xharu/combination/FilterEliminator.java
@@ -52,4 +52,38 @@ public class FilterEliminator implements Eliminator {
         }
         return list;
     }
+
+    /**
+     * 제외수 필터
+     * @param list
+     * @param filterList
+     * @return
+     */
+    public ArrayList<ArrayList> removeByArray(ArrayList<ArrayList> list, ArrayList filterList){
+        for(int k=0; k<filterList.size(); k++){
+            int y[] = (int[])filterList.get(k);
+            int yLength = y.length;
+            for(int i=list.size()-1; i>=0; i--){
+                int count = 0;
+                ArrayList item = list.get(i);
+                boolean flag = false;
+                for(int j=0; j<item.size(); j++){
+                    int x = (int)item.get(j);
+                    for(int m=0; m<yLength; m++){
+                        if(x==y[m]){
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        break;
+                    }
+                }
+                if(flag){
+                    list.remove(i);
+                }
+            }
+        }
+        return list;
+    }
 }
