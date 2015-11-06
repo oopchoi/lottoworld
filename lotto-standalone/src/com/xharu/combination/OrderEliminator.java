e840e9c36f289201572a299945625052f7db2610
oopchoi <oopchoi@gmail.com>
2015-11-06 12:12:13 +0900

    Update OrderEliminator.java

--- lotto-standalone/src/com/xharu/combination/OrderEliminator.java
+++ lotto-standalone/src/com/xharu/combination/OrderEliminator.java
@@ -9,30 +9,38 @@ public class OrderEliminator implements Eliminator {
     @Override
     public ArrayList<ArrayList> remove(ArrayList<ArrayList> list, ArrayList orderCount) {
         for(int i=list.size()-1; i>=0; i--) {
+            // 마지막 행 아이템
             ArrayList item = list.get(i);
             int continueCount = 1;
-            for(int j=0; j<item.size(); j++){
-                int x = (int)item.get(j);
-                if(j>0){
-                    int y = (int)item.get(j-1)+1;
-                    //System.out.println(x + ", " + y);
-                    if(x==y){
-                        continueCount++;
-                        //System.out.println("continueCount:"+continueCount);
-                        if(continueCount==(int)orderCount.get(0)){
-                            break;
-                        }
-                    }else{
-                        continueCount = 1;
+            boolean flag = false;
+            // 들어있는 개수만큼 반복
+            for(int j=1; j<item.size(); j++){
+                // 이전 수
+                int x = (int)item.get(j-1);
+                // 지금 수
+                int y = (int)item.get(j);
+                // 전 수에서 1더한 값이 지금수와 같으면
+                if(x+1==y){
+                    // 카운트 증가
+                    continueCount++;
+                    //System.out.println("continueCount:"+continueCount);
+                    // 카운트가 사용자가 입력한 수와 같으면
+                    if(continueCount==(int)orderCount.get(0)){
+                        // 제거 대상임
+                        flag = true;
+                        break;
                     }
-
+                }else{
+                    continueCount = 1;
                 }
+
             }
-            if(continueCount==(int)orderCount.get(0)){
+            if(flag){
                 list.remove(i);
             }
         }
         return list;
     }
+
 
 }
